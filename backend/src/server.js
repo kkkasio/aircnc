25a1837654759ac2b2f6437d6321c44c73e76fa4
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-10-03 22:31:21 -0300

    frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+const path = require('path');
 
 mongoose.connect(
   'mongodb+srv://aircnc:88222547@aircnc-j1ial.mongodb.net/aircnc?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   }
 );
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
